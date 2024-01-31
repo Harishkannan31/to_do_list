ba9367a0ebf780d64edc6b9161936ea96f313660
Harishkannan31 <107579282+Harishkannan31@users.noreply.github.com>
2024-01-31 17:31:08 +0530

    Add files via upload
    to do list 1st commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// document.querySelector("li").classList.toogle("done");
+// here for done class we should add styling as text decoration:line-through;
+function hello(){
+var hell=document.getElementById("check").value;
+if (hell=="on"){
+    alert(hell);
+
+}
+}
